88eba5d45e18dc0c4bade296ec0997a40ce1cc88
Mayank Mahajan <mayankmahajan9056@gmail.com>
2026-09-01 20:38:56 +0530

    feat(academic): export elective group router in index file

--- apps/api/src/modules/academic/index.ts
+++ apps/api/src/modules/academic/index.ts
@@ -3,3 +3,4 @@ export { programRouter } from './programs/program.routes.js';
 export { curriculumRouter } from './curricula/curriculum.routes.js';
 export { semesterCatalogRouter } from './SemesterCatalog/semester.routes.js';
 export { subjectRouter } from './subjects/subject.routes.js';
+export { electiveGroupRouter } from './electives/elective.routes.js';
