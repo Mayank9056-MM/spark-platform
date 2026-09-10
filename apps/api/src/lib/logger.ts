54783295beb192f2b7b78b4a26d437682db0b812
Mayank Mahajan <mayankmahajan9056@gmail.com>
2026-09-10 17:21:06 +0530

    feat(logger): add promotion logger for enhanced logging

--- apps/api/src/lib/logger.ts
+++ apps/api/src/lib/logger.ts
@@ -32,3 +32,4 @@ export const academicYearLogger = createChildLogger({ component: 'academic-year'
 export const admissionLogger = createChildLogger({ component: 'admission' });
 export const studentEnrollmentLogger = createChildLogger({ component: 'student-enrollment' });
 export const semesterEnrollmentLogger = createChildLogger({ component: 'semester-enrollment' });
+export const promotionLogger = createChildLogger({ component: 'promotion' });
